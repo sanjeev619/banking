d701f5ce562f9460b88f49dda354be1477317ddc
Sanjeev Chinthakunta <sanjeev@skillate.com>
2021-01-25 21:27:05 +0530

    initialize sptring boot web app

---
+++ src/test/java/com/bank/web/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
